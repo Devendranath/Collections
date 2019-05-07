0bf7fa12a3d586ed957a5d0fa0e7d6d85f825898
Devendranath <dnreddy890@gmail.com>
2019-05-07 08:46:36 +0530

    Initial Commit

---
+++ Collections/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Collections
+//
+//  Created by apple on 07/05/19.
+//  Copyright © 2019 iOSProofs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
